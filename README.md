f437c6da6e2b806270363ae5eb1de79d9ae195a6
minmin0530 <izumiyoshiki@icloud.com>
2020-04-08 13:48:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# network-voxel-editor
+多人数同時接続のボクセルエディタ  
+# usage
+'''npm install --save express socket.io'''
+'''node main.js'''
---
+++ main.js
@@ -0,0 +1,66 @@
+const express = require('express');
+
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const users = new Map();
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+    socket.on('connected', data => {
+        users.set(socket.id, {
+            clientID: data.clientID,
+            position: data.position
+        });
+
+        io.emit('connected', {
+            newUser: data,
+            currentUsers: Array.from(users.values()).filter(user => user.clientID !== data.clientID)
+        });
+
+    //     console.log('new client connected:');
+    //     console.log(Array.from(users.values()).reduce((acc, c) => {
+    //         return acc + (c.clientID === data.clientID ? `\u001b[36m${c.clientID}\u001b[0m, ` : `${c.clientID}, `)
+    //     }, '').slice(0, -2))
+    });
+
+    socket.on('put', data => {
+        users.set(socket.id, {
+            clientID: data.clientID,
+            position: data.position
+        });
+
+        io.emit('put', {
+            newUser: data,
+            currentUsers: Array.from(users.values()).filter(user => user.clientID !== data.clientID)
+        });
+
+    });
+
+    socket.on('updatePosition', data => {
+        users.set(socket.id, {
+            clientID: data.clientID,
+            position: data.position
+        });
+        io.emit('updatePosition', data);
+    });
+
+    // socket.on("disconnect",  () => {
+    //     if (users.has(socket.id)) {
+    //         io.emit('disconnected', users.get(socket.id).clientID);
+    //         users.delete(socket.id);
+    //         console.log('client disconnected:');
+    //         console.log(Array.from(users.values()).reduce((acc, c) => {
+    //             return acc + c.clientID + ', '
+    //         }, '').slice(0, -2));
+    //     }
+    // });
+});
+
+http.listen(8080, () => {
+    console.log('listening on :8080');
+});
